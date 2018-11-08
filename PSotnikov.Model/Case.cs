4be61af8d4fff264084cc80533d5a18da48a6faa
Pavel Sotnikov <pavel.sotnikov.dne@gmail.com>
2018-11-08 16:42:13 +0200

    Update2019.1. Case model update.
    Case model update.

--- PSotnikov.Model/Case.cs
+++ PSotnikov.Model/Case.cs
@@ -14,8 +14,10 @@ namespace PSotnikov.Model
        [Required]
        public string CaseName { get; set; }
 
+       // Description of the case
        public string CaseDescription { get; set; }
 
+       // DateTime when case was added into examples set
        public DateTime CreationDT { get; set; }
     }
 }
